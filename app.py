7dba86e18ba6a554431ef4ed330e635bb7bac8c4
Nicholas Njagi <njaginicholas@gmail.com>
2024-02-21 11:05:30 +0300

    update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 from flask_cors import CORS
-from models import db, User, Role
+from models import db, TokenBlocklist
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
 from views import *
 
 load_dotenv()
@@ -17,6 +19,10 @@ CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+jwt = JWTManager()
+jwt.init_app(app)
 db.init_app(app)
 Migrate(app,db)
 
@@ -25,7 +31,16 @@ ADMIN_ROLE_ID = 1
 TEACHER_ROLE_ID = 2
 STUDENT_ROLE_ID = 3
 
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+    jti = jwt_payload["jti"]
+    token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
+
+    return token is not None
+
 api.add_resource(Login,'/login')
+api.add_resource(AuthenticatedUser, '/authenticated_user')
+api.add_resource(Logout, '/logout')
 api.add_resource(AddTeacher,'/addteacher')
 api.add_resource(AddStudent,'/addstudent')
 
