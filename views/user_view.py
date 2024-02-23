a5474c181c1e25c6743aac95ab064a68e0f76a22
Shadrack kipkorir <shadybett540@gmail.com>
2024-02-23 12:17:19 +0300

    Limited the addition of teachers to admin's duty only

--- views/user_view.py
+++ views/user_view.py
@@ -2,12 +2,20 @@ from flask import request, jsonify, make_response
 from flask_restful import Resource, reqparse
 from flask_bcrypt import Bcrypt
 from models import db, User, Role
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 bcrypt = Bcrypt()
 
 
 class AddTeacher(Resource):
+    @jwt_required()
+
     def post(self):
+        user_id = get_jwt_identity()
+        user = User.query.filter_by(id=user_id).first()
+        if user.role_id !=1:
+            return make_response(jsonify({'error': 'Permission denied'}), 403)
+
         parser = reqparse.RequestParser()
         parser.add_argument('first_name', type=str, required=True)
         parser.add_argument('last_name', type=str, required=True)
