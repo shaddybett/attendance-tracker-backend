ad809fb86548a90eb2e4f139163e78a8bde6879b
Shaddy <shadybett540@gmail.com>
2024-07-26 12:49:33 +0300

    done

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 jwt = JWTManager()
 jwt.init_app(app)
 db.init_app(app)
